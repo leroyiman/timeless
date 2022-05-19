ab8bc53ce17da00cde6a947b804a5e11d25dc6fd
Leroy Macartney <leroy.macartney@googlemail.com>
2022-05-19 21:32:35 +0200

    Email

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,10 +1,15 @@
 class Match < ApplicationRecord
   belongs_to :search
   belongs_to :offer
+  has_one :user, :through => :search
+
+
+  # belongs_to :user, :through => :search
 
   after_create :send_email
 
   def send_email
+    p "Mail sended?"
     # variable = Mailjet::Send.create(messages: [{
     #   'From'=> {
     #     'Email'=> 'leroy.macartney@googlemail.com',
@@ -25,17 +30,28 @@ class Match < ApplicationRecord
     # p variable.attributes['Messages']
 
      #what the messages look like:
-    Mailjet::Send.create(messages: [{
+     variable = Mailjet::Send.create(messages: [{
+      'From'=> {
+        'Email'=> 'whatever@google.com',
+        'Name'=> 'Me'
+      },
       "To" => [
         {
-          "Email" => user.email,
-          "Name" => user.email_name
+          "Email" => search.user.email,
+          # Match.first.search.user.email
+          "Name" => "search.user.fname"
+          #Match.first.search.user.fname
         }
       ],
-      "TemplateID" => template_id,
-      "TemplateLanguage" => true,
-      "Variables" => variables
-    }]
-  end
 
+        'Subject'=> 'My first Mailjet Email!',
+        'TextPart'=> 'Greetings from Mailjet!',
+        'HTMLPart'=> '<h3>Dear passenger 1, welcome to <a href=\'https://www.mailjet.com/\'>Mailjet</a>!</h3><br />May the delivery force be with you!'
+      # "TemplateID" => template_id,
+      # "TemplateLanguage" => true,
+      # "Variables" => variables
+    }])
+
+    p variable.attributes['Messages']
+  end
 end
