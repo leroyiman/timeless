178a428c3ba89459a7f2d95a1a1aa02a6fd9f69a
eduardo roldan <eduroldanisaac@gmail.com>
2022-05-24 00:39:26 +0200

    results page and Favorite

---
+++ app/javascript/controllers/favorite_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    console.log("Hello from our first Stimulus controller", this.element)
+  }
+
+  favorited() {
+    // this.element.setAttribute("color", "red");
+    console.log(this.element);
+    this.element.style.color = "red"
+    console.log(this.element);
+  }
+
+  reset() {
+    // this.element.setAttribute("color", "red");
+    console.log(this.element);
+    this.element.style.color = "red"
+    console.log(this.element);
+  }
+
+}
