0c27733f87f4194a6146b37c113e958b5d5459b9
Leroy Macartney <leroy.macartney@googlemail.com>
2022-05-24 10:43:57 +0200

    Bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_05_21_110657) do
+ActiveRecord::Schema.define(version: 2022_05_23_231521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_05_21_110657) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "is_confirmed", default: false
+    t.boolean "is_confirmed"
     t.bigint "user_id", null: false
     t.bigint "timeslot_id", null: false
     t.datetime "created_at", precision: 6, null: false
